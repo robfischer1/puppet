cd3ea9029979edf8a2695f3b36f513a0cc0eb892
robfischer1 <robfischer1@gmail.com>
2016-03-05 02:19:44 -0500

    Move mount to /opt

--- rob_sabnzbd/manifests/init.pp
+++ rob_sabnzbd/manifests/init.pp
@@ -2,38 +2,42 @@ include 'docker'
 include 'firewall'
 
 class rob_sabnzbd {
-        firewall {
-                '052 Sabnzbd':
-                        proto  => 'tcp',
-                        dport  => [8080, 9090],
-                        action => 'accept';
-        }
+	firewall {
+		'052 Sabnzbd':
+			proto  => 'tcp',
+			dport  => [8080, 9090],
+			action => 'accept';
+	}
 
-        file {
-                '/home/sabnzbd/config':
-                        ensure => directory,
-                        mode   => 0775,
-                        owner  => sabnzbd,
-                        group  => media;
+	::nfs::client::mount { 
+		'/opt/sabnzbd':
+			server  => '10.0.7.3',
+			share   => '/mnt/stor0/users/home/sabnzbd',
+			mount   => '/opt/sabnzbd',
+			#options => 'rw,context=unconfined_u:object_r:user_home_t:s0',
+			options => 'rw,nosharecache,context=system_u:system_r:svirt_sandbox_file_t:s0';
+	}
+	
+	file {
+		'/opt/sabnzbd/config':
+			ensure => directory,
+			mode   => 0775,
+			owner  => sabnzbd,
+			group  => media;
 
-                '/home/sabnzbd/downloads':
-                        ensure => directory,
-                        mode   => 0775,
-                        owner  => sabnzbd,
-                        group  => media;
-        }
+		'/opt/sabnzbd/downloads':
+			ensure => directory,
+			mode   => 0775,
+			owner  => sabnzbd,
+			group  => media;
+	}
 
-        docker::run {
-                'sabnzbd':
-                        image           => 'linuxserver/sabnzbd',
-                        ports           => ['8080:8080', '9090:9090'],
-                        volumes         => ['/home/sabnzbd/config:/config:Z', '/home/sabnzbd/downloads:/downloads:z'],
-                        privileged      => true,
-                        #volumes         => ['10.0.7.3/mnt/stor0/users/home/sabnzbd/config:/config:Z', '10.0.7.3/mnt/stor0/users/home/sabnzbd/downloads:/downloads:z'],
-                        #volumes         => ['/opt/config:/config:Z', '/home/sabnzbd/downloads:/downloads:z'],
-                        #volumes         => ['/home/sabnzbd/downloads:/downloads:z'],
-                        env             => ['PUID=140800017', 'PGID=140800014'],
-                        #extra_parameters => ['--volume-driver=nfs'];
-        }
+	docker::run {
+		'sabnzbd':
+			image           => 'linuxserver/sabnzbd',
+			ports           => ['8080:8080', '9090:9090'],
+			volumes         => ['/opt/sabnzbd/config:/config', '/opt/sabnzbd/downloads:/downloads'],
+			env             => ['PUID=140800017', 'PGID=140800014'];
+	}
 
 }
